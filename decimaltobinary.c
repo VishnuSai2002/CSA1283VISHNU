cde4f17a2d0eb52497975855940b22cac2a95754
VishnuSai2002 <112846517+VishnuSai2002@users.noreply.github.com>
2022-09-15 21:51:22 +0530

    Add files via upload

---
+++ decimaltobinary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a[10],n,i;
+printf("enter the number to convert:");
+scanf("%d",&n);
+for(i=0;n>0;i++)
+{
+a[i]=n%2;
+n=n/2;
+}
+printf("binary of given number is:");
+for(i=i-1;i>=0;i--)
+printf("%d",a[i]);
+
+}
